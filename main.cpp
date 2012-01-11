3a625f67aefd9360f958cadf9a109d43cd912c44
Daniel Neil Hodder <danielh.canberra@gmail.com>
2012-01-12 02:07:44 +1100

    Additional comments in the smoketests.

--- main.cpp
+++ main.cpp
@@ -21,6 +21,18 @@
 // This program smoke tests the LibIPvX objects.
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	std::cout << "        _____ ______  _____  _____  _    _ _     _" << std::endl; 
+	std::cout << " |        |   |_____]   |   |_____]  \\  /   \\___/ " << std::endl; 
+	std::cout << " |_____ __|__ |_____] __|__ |         \\/   _/   \\_" << std::endl; 
+ 
+
+	std::cout << std::endl;
+	std::cout << "***************************" << std::endl;
+	std::cout << "Starting smoke tests." << std::endl;
+	std::cout << "***************************" << std::endl;
+	std::cout << std::endl;
+
 	std::auto_ptr<IP4Address> address(new IP4Address("10.1.5.2"));
 
 	// Check that the default netmask has been assigned - this value is automatically
@@ -29,89 +41,114 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	assert( address->netmask == IPV4_CLASS_A_DEFAULT_SUBNET_MASK);
 	std::cout << "Default subnet mask: " << address->GetDefaultNetmask() << std::endl;
-
+	std::cout << std::endl;
+	
 	// We're using a default netmask, therefore we're not subnetting.
 	assert( !address->IsSubnetted() );
 	if ( !address->IsSubnetted() ) {
 		std::cout << "IP Address: " << address->GetAddressString() << " is not subnetted." << std::endl;
 	}
-
+	std::cout << std::endl;
+	
 	// Now set a subnet netmask - this replaces the default subnet mask.
 	address->netmask = "255.255.128.0";
-
+	
 	// Display the binary of the IP's address space.
 	std::cout << "Address binary: " << address->GetAddressBitset() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Display the subnet address.
 	assert( address->GetSubnetAddress() == "10.1.0.0" );
 	std::cout << "Subnet address: " <<  address->GetSubnetAddress() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Display the broadcast address.
 	assert( address->GetBroadcastAddress() == "10.1.127.255" );
 	std::cout << "Broadcast address: " << address->GetBroadcastAddress() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Is this address a broadcast address, no.
 	assert( !address->IsBroadcastAddress() );
 	if ( !address->IsBroadcastAddress() ) {
 		std::cout << address->GetAddressString() << " is not a broadcast address." << std::endl;
 	}
-
+	std::cout << std::endl;
+	
 	// Is this address a subnet address, no.
 	assert( !address->IsSubnetAddress() );
 	if ( !address->IsSubnetAddress() ) {
 			std::cout << address->GetAddressString() << " is not a subnet address." << std::endl;
 	}
-
+	std::cout << std::endl;
+	
 	// Get the address as formatted decimal string.
 	assert( address->GetAddressString() == "10.1.5.2" );
 	std::cout << "IP: " << address->GetAddressString() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the netmask in CIDR notation.
 	assert( address->GetNetmaskBitLength() == 17 );
 	std::cout << "Netmask in CIDR notation: " << address->GetNetmaskBitLength() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get each Octet's binary using an array.
 	for ( size_t i = 0; i < IPV4_OCTET_COUNT; i++ ) {
 		std::cout << "Fetching Octet's binary in array at index " << i << ": " << address->GetOctetBinaryByIndex(i) << std::endl;
 	}
-
+	std::cout << std::endl;
+	
 	// Get each Octet's decimal using an array.
 	for ( size_t i = 0; i < IPV4_OCTET_COUNT; i++ ) {
 		std::cout << "Fetching Octet's decimal in array at index " << i << ": " << address->GetOctetDecimalByIndex(i) << std::endl;
 	}
-
+	std::cout << std::endl;
+	
 	// Get the first Octet in binary.
 	std::cout << "First octet's binary: " << address->GetFirstOctetBitset() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the first Octet in decimal.
 	assert( address->GetFirstOctetDecimal() == 10);
 	std::cout << "First octet's decimal: " << address->GetFirstOctetDecimal() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the second Octet in binary.
 	std::cout << "Second octet's binary: " << address->GetSecondOctetBitset() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the second Octet in decimal.
 	assert( address->GetSecondOctetDecimal() == 1);
 	std::cout << "Second octet's decimal: " << address->GetSecondOctetDecimal() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the third Octet in binary.
 	std::cout << "Third octet's binary: " << address->GetThirdOctetBitset() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the third Octet in decimal.
 	assert( address->GetThirdOctetDecimal() == 5);
 	std::cout << "Third octet's decimal: " << address->GetThirdOctetDecimal() << std::endl;
-
+	std::cout << std::endl;
+	
 	// Get the fourth Octet in binary.	
 	std::cout << "Fourth octet's binary: " << address->GetFourthOctetBitset() << std::endl;
+	std::cout << std::endl;
 
 	// Get the fourth Octet in decimal.
 	assert( address->GetFourthOctetDecimal() == 2);
 	std::cout << "Fourth octet's decimal: " << address->GetFourthOctetDecimal() << std::endl;
+	std::cout << std::endl;
 
 	// Get the IP Address's class.
 	assert( address->GetClass() == IPV4_ADDRESS_CLASS_A );
 	std::cout << "Address's class is: " << address->GetClass() << std::endl;
 	
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "***************************" << std::endl;
+	std::cout << "All tests were successful." << std::endl;
+	std::cout << "***************************" << std::endl;
+
 	return 0;
 }
 
